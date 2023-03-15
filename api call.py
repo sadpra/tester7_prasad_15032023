bbab5792b42181904ff566b4209538356832c1ed
unknown <Test@PNI6W11289.net.plm.eds.com>
2023-03-15 13:16:27 +0530

    4th commit

---
+++ api call.py
@@ -0,0 +1,10 @@
+{
+    'X-Siemens-Auth': 'test'
+}
+payload = {
+  "subnet_id": "aws_vpc.vpc.id",
+  "name": "Prasad",
+  "email": "p2deshmukh1815@gmail.com"
+}
+
+
--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
 provider "aws" {
+  region  = "eu-west-1" 
+
+data "S3" {
     S3 Bucket: "3.devops.candidate.exam"
     Region: "eu-west-1"
     Key: "Prasad.Deshmukh"
@@ -48,6 +51,32 @@ resource "aws_route_table" "my-route" {
   }
 }
 
+resource "aws_security_group" "SG" {
+  name        = "security_group"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["10.0.0.0/16"]
+    ipv6_cidr_blocks = [aws_vpc.vpc.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "sg"
+  }
+}
 
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.subnet-1.id
